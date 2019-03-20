ca222d7a868dafe4e4d540a1df05a0d858659110
Christopher Goh <chrisgzf@gmail.com>
2019-03-21 03:43:28 +0800

    Fixed pset4 speller linkedList insertion function

--- pset4/speller/dictionary.c
+++ pset4/speller/dictionary.c
@@ -23,7 +23,8 @@ node;
 // Represents a hash table
 node *hashtable[N];
 
-void insert(node *linkedList, node *i);
+void insert(node **hashTable, int hash, node *i);
+unsigned int getSize(node *n);
 
 // Hashes word to a number between 0 and 25, inclusive, based on its first letter
 unsigned int hash(const char *word)
@@ -58,14 +59,12 @@ bool load(const char *dictionary)
         node *i = malloc(sizeof(node));
         strcpy(i->word, word);
         i->next = NULL;
-        insert(hashtable[hash(word)], i);
+        insert(hashtable, hash(word), i);
     }
 
     // Close dictionary
     fclose(file);
-    while (true){
-        printf("Fuck yeah");
-    }
+    printf("[INFO] Dictionary Loaded\n");
 
     // Indicate success
     return true;
@@ -75,13 +74,21 @@ bool load(const char *dictionary)
 unsigned int size(void)
 {
     // TODO
-    return 0;
+    unsigned int nWords = 0;
+    for (int i = 0; i < N; i++)
+    {
+        nWords += getSize(hashtable[i]);
+    }
+    printf("[INFO] %d words in dictionary\n", nWords);
+    return nWords;
 }
 
 // Returns true if word is in dictionary else false
 bool check(const char *word)
 {
     // TODO
+    int wHash = hash(word);
+
     return false;
 }
 
@@ -94,10 +101,28 @@ bool unload(void)
 
 
 // Sets the new node to point at head, set new node to be new head
-void insert(node *linkedList, node *i)
+void insert(node **hashTable, int hash, node *i)
 {
     // Haha, somehow this thing handles NULL as well (i.e. when linkedlist is empty)
-    i->next = linkedList;
-    linkedList = i;
+    i->next = hashTable[hash];
+    hashTable[hash] = i;
     return;
 }
+
+
+unsigned int getSize(node *n)
+{
+    if (n == NULL)
+    {
+        return 0;
+    }
+    if (n->next != NULL)
+    {
+        return getSize(n->next) + 1;
+    }
+    else
+    {
+        return 1;
+    }
+}
+
